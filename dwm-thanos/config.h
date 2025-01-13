0d32db9030110090e80609c5c57861f754af74cc
Thanos Antonopoulos <thanos@tinos.home>
2025-01-13 22:29:16 +0200

    debian-thanos

--- dwm-thanos/config.h
+++ dwm-thanos/config.h
@@ -21,7 +21,7 @@ static const char normbordercolor[]           = "#3B4252";
 static const char normbgcolor[]               = "#2E3440";
 static const char normfgcolor[]               = "#D8DEE9";
 static const char selbordercolor[]            = "#EBCB8B";
-static const char selbgcolor[]                = "#434C5E";
+static const char selbgcolor[]                = "#5E81AC";
 static const char selfgcolor[]                = "#ECEFF4";
 
 static const char *colors[][3] = {
